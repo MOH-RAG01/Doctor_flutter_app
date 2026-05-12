8426d1d707a3f2a8b6574fe579d48ff44002ae3b
Mohamed Ragab <mohamedmicro20@outlook.com>
2026-05-12 10:06:57 +0300

    - added fastlane to android ,and connected it to Firebase App distribution - bumped version to 1.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:doctor/core/routing/app_router.dart';
 import 'package:doctor/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   // AppFlavorsConfig.initialize();
