f27a624d61dd44c18021587e9bfe0faa5de9c9fd
Mohamed Ragab <mohamedmicro20@outlook.com>
2026-08-23 14:08:39 +0300

    - created a shared_pref_helper to be able to save and read token or any other data need to be saved - created a constant key for the shared_pref to help read easily - modfiy the login cubit on success condition to save the token - added a saveUserToken function to save the token and updated it in the first login - added headers to the dio factory to send the token on every request and updated the token after first login - create a function and a variable to check if the user logged in or not and call it in the main - add the the logged in variable to the constructor and check the initial route upon it - add the secure storage fultter package

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,12 +1,15 @@
 import 'package:doctor/core/routing/app_router.dart';
 import 'package:doctor/core/routing/routes.dart';
 import 'package:doctor/core/theming/app_colors.dart';
+import 'package:doctor/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
   final AppRouter appRouter;
-  const DocApp({super.key, required this.appRouter});
+  final bool isLoggedIn;
+
+  const DocApp({super.key, required this.appRouter, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,7 @@ class DocApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.white,
         ),
         debugShowCheckedModeBanner: false,
-        initialRoute: Routes.homeScreen,
+        initialRoute: isLoggedIn ? Routes.homeScreen : Routes.loginScreen,
         onGenerateRoute: appRouter.generateroute,
       ),
     );
